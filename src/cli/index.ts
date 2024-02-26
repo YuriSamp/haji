4bf19cb8ce2b0744ad50dc02a93ecc16dd7d9c7d
Yuri <yurisamp123@gmail.com>
2024-02-26 17:25:44 -0300

    add helpers function to check name of the project

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -1,3 +1,10 @@
+import chalk from "chalk";
+
+import {
+  checkIfFolderIsEmpty,
+  getFullPath,
+  isProjectNameValid,
+} from "../helpers/index.js";
 import {
   promptPackageManager,
   promptProjectName,
@@ -6,8 +13,25 @@ import {
 } from "./prompts.js";
 
 export const runCli = async () => {
-  await promptProjectName();
-  await promptWithType();
-  await promptWithTest();
-  await promptPackageManager();
+  const name = await promptProjectName();
+  const fullPath = getFullPath(name);
+  const isEmpty = await checkIfFolderIsEmpty(fullPath);
+
+  if (!isProjectNameValid(name)) {
+    console.error(
+      `❌ ${chalk.red("Invalid project name! It should contains only letters, numbers, dashes and underscores.")}`
+    );
+    process.exit(0);
+  }
+
+  if (!isEmpty) {
+    console.error(`❌ ${chalk.red("Folder is not empty!")}`);
+    process.exit(0);
+  }
+
+  const projectType = await promptWithType();
+  const withTests = await promptWithTest();
+  const packageManager = await promptPackageManager();
+
+  return { name, projectType, withTests, packageManager };
 };
