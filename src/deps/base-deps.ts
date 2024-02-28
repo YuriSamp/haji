e905790d319e3fec93470060431482da8af7e382
Yuri <yurisamp123@gmail.com>
2024-02-28 00:56:34 -0300

    update base deps

--- src/deps/base-deps.ts
+++ src/deps/base-deps.ts
@@ -1,1 +1,15 @@
-export const baseDeps = ["tsx", "typescript", "@types/node", "tsup"];
+export const baseDeps = [
+  "tsx",
+  "typescript",
+  "@types/node",
+  "tsup",
+  "@ianvs/prettier-plugin-sort-imports",
+  "@typescript-eslint/eslint-plugin",
+  "@typescript-eslint/parser",
+  "eslint",
+  "eslint-config-prettier",
+  "eslint-plugin-import",
+  "eslint-plugin-isaacscript",
+  "eslint-plugin-prettier",
+  "prettier"
+];
